61e8cf35f1bac01b7768b8e7a28453de1280ade8
dinerograham <102576939+dinerograham@users.noreply.github.com>
2023-11-09 22:43:24 +0000

    square works

---
+++ app/controllers/square_controller.rb
@@ -0,0 +1,13 @@
+class SquareController < ApplicationController
+  
+  def show_square_form
+    render({:template => "square_templates/square_form"})
+  end
+
+  def square_this
+    @number = params.fetch("root").to_f
+    @square = @number**2
+    render({:template => "square_templates/square_results"})
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   
+  get("/", {:controller => "square", :action => "show_square_form"})
+
+  get("/square/new", {:controller => "square", :action => "show_square_form"})
+  get("/square/results", {:controller => "square", :action => "square_this"})
+
+  get("/square_root/new", {:controller => "square_root", :action => "show_sqrt_form"})
+  get("/square_root/results", {:controller => "square_root", :action => "find_sqrt"})
+
+  get("/payment/new", {:controller => "payment", :action => "show_payment_form"})
+  get("/payment/results", {:controller => "payment", :action => "calculate_payment"})
+
+  get("/random/new", {:controller =>"random", :action => "show_random_form"})
+  get("/random/results", {:controller =>"random", :action => "pick_random"})
 end
